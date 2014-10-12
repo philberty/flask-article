d58864def1c3658860613a00e3ee1614ed75516a
redbrain <philip.herron@instil.co>
2014-10-12 20:09:11 +0100

    cleanup

--- www/js/app.js
+++ www/js/app.js
@@ -1,50 +1,20 @@
 require.config({
     paths: {
-	jquery: '/js/lib/jquery/dist/jquery',
-	flot: '/js/lib/flot/jquery.flot',
         angular: '/js/lib/angular/angular',
-        angularRoute: '/js/lib/angular-route/angular-route',
     },
     shim: {
-	'flot': {
-	    deps: ['jquery'],
-	    exports: 'flot'
-	},
-        'angularRoute': {
-            deps: ['angular'],
-            exports: 'angular'
-        },
         'angular': {
-	    deps: ['jquery'],
             exports: 'angular'
         }
     },
     deps: ['app']
 });
 
-define('app', ["flot", "angular", "angularRoute"], function($, angular)
+define('app', ["angular"], function(angular)
 {
-    var app = angular.module("StatsApp", ['ngRoute']);
-    
-    app.config(
-        ['$routeProvider',
-            function($routeProvider) {
-                $routeProvider
-                    .when('/host', {
-                        templateUrl: 'host.html',
-                        controller: 'host'
-                    })
-                    .when('/', {
-                        redirectTo: "/host"
-                    })
-                    .otherwise({
-                        redirectTo: '/'
-                    })
-            }
-        ]
-    )
+    var app = angular.module("StatsApp", []);
 
-    app.controller('host', function($scope, $interval, $http) {
+    app.controller('app', function($scope, $interval, $http) {
 	var hostInfo = function() {
 	    $http.get('/api/hostinfo').success(function(data) {
 		$scope.info = data
